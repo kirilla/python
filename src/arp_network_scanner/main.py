3d211ef5d7a2c33cb46905e20888a6ad2a04199b
Jonas Sundstrom <jonas@kirilla.se>
2024-02-13 16:52:42 +0000

    Import scapy, add some error feedback.

---
+++ src/arp_network_scanner/main.py
@@ -0,0 +1,16 @@
+from scapy.all import *
+
+INTERFACE = "eth0"
+IP_RANGE = "10.10.x.x/24"
+BROADCAST_MAC = "ff:ff:ff:ff:ff:ff"
+
+def main():
+    packet = Ether(dst=BROADCAST_MAC)/ARP(pdst=IP_RANGE)
+
+    answered, unanswered = srp(packet, timeout=2, iface=INTERFACE, inter=0.1)
+    
+    for send, receive in answered:
+        print(receive.sprintf(r"%Ether.src% - %ARP.psrc%"))
+
+if __name__ == "__main__":
+    main()
